c9a71c74ad906afa375cf6e38d3ae915745e9063
Abhinandan Mukherjee <amukherjeehowrah@gmail.com>
2023-09-03 10:53:21 +0530

    2nd commit on 03/09/2023

--- Oops/inheritPrac2.java
+++ Oops/inheritPrac2.java
@@ -9,6 +9,9 @@ public class inheritPrac2 {
         Nexon.speed = 120;
         System.out.println("The speed is " + Nexon.speed);
         Nexon.charging();
+        
+        Cars c = new Cars();
+        c.maintenance(); // calls the overridden method
 
     }
     
@@ -30,6 +33,9 @@ class Cars extends Vehicles{
     int speed;
     void insurance(){
         System.out.println("It requires car insurance for protection");
+    }
+    void maintenance(){   // method overriding (an example of runtime or dynamic polymorphism)
+        System.out.println("Need to go to the car mechanic for maintenance");
     }
 }
 
