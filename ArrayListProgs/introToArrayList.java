925da516f99e85f9831be5434f7f56e3f7861246
Abhinandan Mukherjee <amukherjeehowrah@gmail.com>
2023-11-03 11:07:44 +0530

    1st commit on 03/11/2023

---
+++ ArrayListProgs/introToArrayList.java
@@ -0,0 +1,9 @@
+package ArrayListProgs;
+import java.util.ArrayList;
+
+public class introToArrayList {
+    public static void main(String args[]){
+        // creating an ArrayList of Integer type, similarly we can also create ArrayList of String/Boolean/Float classes.
+        ArrayList<Integer> myList = new ArrayList<>();
+    }
+}
